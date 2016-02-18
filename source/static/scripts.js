6eb5395dc3bed232e686c42c1fc6b26e75232fd9
Claudio Mendonca <claudioccm@gmail.com>
2016-02-18 11:26:51 -0500

    added js functionality for filter-panel expand

--- source/static/scripts.js
+++ source/static/scripts.js
@@ -1,18 +1,22 @@
 $(document).ready(function() {
 
+$('.js-filter-trigger').click(function() {
+    $('.js-filter-panel').toggleClass('js-is-open');
+});
+
 // Slider Config (Sticky)
-    $('.slider').slick({
-        arrows: true,
-        draggable: false,
-        swipeToSlide: true,
-        responsive: [
-            {
-                breakpoint: 800,
-                settings: {
-                    draggable: true,
-                }
+$('.slider').slick({
+    arrows: true,
+    draggable: false,
+    swipeToSlide: true,
+    responsive: [
+        {
+            breakpoint: 800,
+            settings: {
+                draggable: true,
             }
-        ]
-    });
+        }
+    ]
+});
 
 });
