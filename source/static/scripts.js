9b6762234d45e6ff9ff635d748a391122487fda6
Claudio Mendonca <claudioccm@gmail.com>
2016-03-07 09:49:59 -0500

    added list/grid view for results.

--- source/static/scripts.js
+++ source/static/scripts.js
@@ -3,6 +3,27 @@ $(document).ready(function() {
     $('.js-filter-panel').toggleClass('js-is-open');
   });
 
+// Projects Grid/List View Logic
+  $('.js-view-list-trigger').click(function() {
+      // Switch Classes for the Project Listing
+      $('.js-results-list').addClass('js-is-list');
+      $('.js-results-list').removeClass('js-is-grid');
+
+      // Adds 'active' state to the button
+      $(this).addClass('js-is-active');
+      $('.js-view-grid-trigger').removeClass('js-is-active');
+  });
+
+  $('.js-view-grid-trigger').click(function() {
+      // Switch Classes for the Project Listing
+      $('.js-results-list').addClass('js-is-grid');
+      $('.js-results-list').removeClass('js-is-list');
+
+      // Adds 'active' state to the button
+      $(this).addClass('js-is-active');
+      $('.js-view-list-trigger').removeClass('js-is-active');
+  });
+
   // Slider Config (Sticky)
   $('.slider').slick({
     arrows: true,
